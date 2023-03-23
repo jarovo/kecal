d8632a85da21ceaa6a7600ffb425867421bd91d3
Jaroslav Henner <1187265+jarovo@users.noreply.github.com>
2023-03-23 17:33:05 +0100

    Multiconn server.

--- kecal/__init__.py
+++ kecal/__init__.py
@@ -1,2 +1,18 @@
+import sys
+import socket
+import selectors
+import types
+
 def main():
-    print("Hello world!")
+    # multiconn-server.py
+    sel = selectors.DefaultSelector()
+
+    # ...
+
+    host, port = '127.0.0.1', 8888
+    lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    lsock.bind((host, port))
+    lsock.listen()
+    print(f"Listening on {(host, port)}")
+    lsock.setblocking(False)
+    sel.register(lsock, selectors.EVENT_READ, data=None)
