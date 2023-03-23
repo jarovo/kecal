6514f7353a40e94d4067daac5d6dd6d703b48559
Jaroslav Henner <1187265+jarovo@users.noreply.github.com>
2023-03-23 17:53:46 +0100

    Client server.

--- tests/test_pychater.py
+++ tests/test_pychater.py
@@ -1,5 +1,6 @@
 import subprocess
 
 def test_chatter():
-    process = subprocess.check_output("kecal", shell=True)
-    assert process == b"Hello world!\n"
+    server_process = subprocess.Popen("kecal", shell=True)
+    client_process = subprocess.Popen("kecal", shell=True)
+    assert server_process == b"Listening on (127.0.0.1', 8888)\n"
