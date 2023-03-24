1def1a9b5c02fc76478c0aa475b8c52bd6a3f79f
Jaroslav Henner <1187265+jarovo@users.noreply.github.com>
2023-03-24 12:19:34 +0100

    Circleci (#4)
    Use Circleci to CI the project.

---
+++ kecal/server.py
@@ -0,0 +1,36 @@
+import argparse
+import asyncio
+
+import kecal
+
+
+async def handle_echo(reader, writer):
+    data = await reader.read(100)
+    message = data.decode()
+    addr = writer.get_extra_info('peername')
+
+    print(f"Received {message!r} from {addr!r}")
+
+    print(f"Send: {message!r}")
+    writer.write(data)
+    await writer.drain()
+
+    print("Close the connection")
+    writer.close()
+    await writer.wait_closed()
+
+
+async def async_main():
+    args = kecal.make_argparser().parse_args()
+
+    server = await asyncio.start_server(
+        handle_echo, args.server_ip, args.server_port)
+
+    addrs = ', '.join(str(sock.getsockname()) for sock in server.sockets)
+    print(f'Serving on {addrs}')
+
+    async with server:
+        await server.serve_forever()
+
+def main():
+    asyncio.run(async_main())
--- tests/test_pychater.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-def test_chatter():
-    process = subprocess.check_output("kecal", shell=True)
-    assert process == b"Hello world!\n"
